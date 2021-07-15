71e39dfdbee8ba21fc7972e19e6ff64584adc71a
tsunga59 <tatsumochi15@gmail.com>
2021-07-15 16:52:31 +0900

    リスト削除機能を作成

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,6 +1,6 @@
 class ListController < ApplicationController
 
-    before_action :set_list, only: %i(edit update)
+    before_action :set_list, only: %i(edit update destroy)
 
     def new
         @list = List.new
@@ -25,6 +25,11 @@ class ListController < ApplicationController
         else
             render action: :edit
         end
+    end
+
+    def destroy
+        @list.destroy
+        redirect_to :root
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
 
-  resources :list, only: %i{new create edit update}
+  resources :list, only: %i{new create edit update destroy}
 end
